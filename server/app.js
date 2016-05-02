02ccf19ec614dec542796b6ea8626c46da42c070
Viktar_Parashchanka <gxvint@gmail.com>
2016-05-02 10:09:13 +0300

    Replace custom route with express

--- server/app.js
+++ server/app.js
@@ -1,2 +1,25 @@
-var server = require("./components/server");
-server();
+'use strict';
+
+let express = require("express"),
+    cors = require('cors'),
+    bodyParser = require('body-parser'),
+    calculate = require('./components/calculate'),
+    app = express(),
+    port = 1222;
+
+app.
+    use(cors()).
+    use(bodyParser.json())
+    .post('/prime-number', function (req, res) {
+        var start = Date.now();
+        var result = calculate.findByMaxValue(req.body.maxValue);
+        var end = Date.now();
+
+        var data = JSON.stringify({
+            number: result,
+            time: end-start
+        });
+        res.end(data);
+    });
+
+app.listen(port, () => {console.log(`${new Date()} Listening at ${port}`);});
--- test/e2e/server.js
+++ test/e2e/server.js
@@ -1,34 +1,30 @@
-var server = require('../../server/components/server'),
+var server = require('../../server/app'),
     request = require('request'),
     expect = require("chai").expect;
 
 describe('server', function () {
-    before(function () {
-        server();
-    });
-
-    describe('/prime-number', function () {
-        it('should return 404 available just post method', function (done) {
-            request.get('http://localhost:3000/prime-number', function (err, res) {
+    describe('route /prime-number', function () {
+        it('should return 404 for GET available just POST method', function (done) {
+            request.get('http://localhost:1222/prime-number', function (err, res) {
                 expect(res.statusCode).to.equal(404);
                 done();
             });
         });
 
-        it('should return 200 or 301', function (done) {
+        it('should return 200 for POST method', function (done) {
             request.post({
-                url:'http://localhost:3000/prime-number',
+                url:'http://localhost:1222/prime-number',
                 json: {maxValue: 3}
             }, function(err,res,body){
-                expect([200, 301]).to.include(res.statusCode);
+                expect([200]).to.include(res.statusCode);
                 done();
             });
         });
 
-        it('should should return 3571 for number 3571', function (done) {
+        it('should return 3571 for number 3571', function (done) {
             var value = 3571;
             request.post({
-                url:'http://localhost:3000/prime-number',
+                url:'http://localhost:1222/prime-number',
                 json: {maxValue: value}
             }, function(err,res,body){
                 console.log(body);
@@ -42,7 +38,7 @@ describe('server', function () {
 
     describe('/any-unexpected-url', function () {
         it('should return 404', function (done) {
-            request.get('http://localhost:3000/any-unexpected-url', function (err, res) {
+            request.get('http://localhost:1222/any-unexpected-url', function (err, res) {
                 expect(res.statusCode).to.equal(404);
                 done();
             });
